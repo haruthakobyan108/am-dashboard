ad750cad76a6adcf27b4a49f1d2af9b2a1b8c383
Harutyun Hakobyan <harutyun.hakobyan@vivaromedia.am>
2022-11-29 15:11:09 +0400

    update ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,25 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ReportRequest;
+use App\Http\Resources\EmptyResource;
 use App\Http\Resources\ReportResource;
 use App\Models\Report;
 use Carbon\Carbon;
-use http\Env\Response;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use JetBrains\PhpStorm\NoReturn;
 
 class ReportController extends Controller
 {
     /**
-     * @param Request $request
-     * @return AnonymousResourceCollection
+     * @param ReportRequest $request
+     * @return AnonymousResourceCollection|EmptyResource
      */
-    #[NoReturn] public function getStreamerReport(ReportRequest $request): AnonymousResourceCollection
+    #[NoReturn] public function getStreamerReport(ReportRequest $request): ReportResource|EmptyResource
     {
         $diff = Carbon::parse($request->input('from'))->diffInDays( $request->input('to'));
         if($diff > 31 ){
-            return response()->json(['message' => 'the date interval can\'t be grader then 31 days ']);
+            return new EmptyResource(['message' => 'report interval can\'t  be more then 31 days']);
         }
         return ReportResource::collection(Report::getStreamerReport($request->input('from'), $request->input('to')));
     }
